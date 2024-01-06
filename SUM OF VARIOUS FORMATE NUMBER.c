3c7a01d6239358f0d9c14dbd06f1ff9cb81ae634
IMRAFI404 <155842367+IMRAFI404@users.noreply.github.com>
2024-01-07 00:38:30 +0600

    Update SUM OF VARIOUS FORMATE NUMBER.c

--- SUM OF VARIOUS FORMATE NUMBER.c
+++ SUM OF VARIOUS FORMATE NUMBER.c
@@ -1,1 +1,37 @@
+/*CREATE A PROGRAM THAT TAKES AN INTEGER NUMBER,AN FLOAT NUMBER AND A DOUBLE NUMBER.THEN SUM THOSE NUMBER AND PRINT THE ANSWER
+*/
+/* CREATOR NAME:IFTAMAM MORTAZA RAFI */
+
+//LETS START :)
+
+#include <stdio.h>
+int main (){
+    int num2;
+    float num1;
+    double num3;
+    float sum;
+
+    //INPUT THE FLOAT FORMATE NUMBER
+    printf("PLEASE ENTER A FLOAT NUMBER: ");
+    scanf("%f",&num1);
+
+    //INPUT THE INTEGER FORMATE NUMBER
+    printf("PLEASE ENTER An INTEGER NUMBER: ");
+    scanf("%D",&num2);
+
+    //INPUT THE DOUBLE FORMATE NUMBER
+    printf("PLEASE ENTER A DOUBLE NUMBER: ");
+    scanf("%lf",&num3);
+
+    //CALCULATION OF SUM
+    sum = num2+num1+num3;
+
+    //PRINTING THE RESULT OF SUMATION
+    printf("THE SUM IS: %f",sum);
+
+    getch();
+    return 0;
+
+}
+
 
